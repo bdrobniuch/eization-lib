e591ff7d734453899f9ee9662d6c70d611bc6a9e
bdrobniuch <drobniuch@gmail.com>
2018-11-14 23:27:16 +0100

    with shuffle

--- eization.js
+++ eization.js
@@ -1,11 +1,7 @@
 let  tab1= [    
-    [["C-","F7"],
-    ["D-","G7"]],
-    [["6"],
-    ["9"]],
-    [["b9"],
-    ["13"],
-    ["9"]]
+    {shuffle: false, list: [ ["C-","F7"] , ["D-","G7"] ]},
+    {shuffle: true, list: [ ["6"] , ["9"] ]},
+    {shuffle: true, list: [ ["b9"] , ["13"], ["9"] ]}
   ];
 
 let tab2= [
@@ -13,7 +9,7 @@ let tab2= [
   [["Maj"],["min"],["7"]]
 ];
   
-
+/*
 function parse(t) {
   let tab=[[]];
   t.forEach((e,i)=> {
@@ -25,42 +21,78 @@ function parse(t) {
         });  
     });
     tab= tempt;
+
   });
   console.log(tab);
 }
+*/
 
-parse(tab1);
-parse(tab2);
+class Shuffle {
+  constructor (exe) {
+    if (exe.shuffle) {
+      this.list = exe.list.slice();
+
+      for (let i = this.list.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.list[i], this.list[j]] = [this.list[j], this.list[i]];
+      }
+
+    } else
+    {
+      this.list = exe.list;
+    }
+  }
+}
+
+class Practice {
+  constructor (ex,tp) {
+    let tab=[[]];
+    ex.forEach((e)=> {
+      let tempt=[];
+      tab.forEach((t1e)=>{
+        let es = new Shuffle(e);
+        es.list.forEach((t2e)=>{
+          tempt.push(t1e.concat([t2e]));
+        });  
+      });
+      tab= tempt;
+    });
+
+    //obraz
+    let out = [];
+    tab.forEach((t1e)=>{
+      let line = [];
+      tp.forEach((t2e)=>{
+        line.push(t1e[t2e.tab][t2e.col]);
+      });
+      out.push(line);
+    });
+    this.output = out;
+  }
+}
+
+
 
 let object1 =
 [
   {
-    t: 0,
+    tab: 0,
     col: 0,
   },
   {
-    t: 1,
+    tab: 1,
     col: 0,
   },
   {
-    t: 0,
+    tab: 0,
     col: 1,
   },
   {
-    t: 2,
+    tab: 2,
     col: 0,
   }
 ];
 
-function print(out) {
-  out.forEach((line) => {
-    let sum= "";
-    line.forEach((elem) => {
-      sum += elem;
-    });
-    console.log(sum);
-    });
-};
+let p1 = new Practice(tab1,object1);
+console.log(p1.output);
 
-
-//print(output);
